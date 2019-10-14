70e0322a68fcfc134e6c761c9319972c7859bcf3
Lucas <lucas208@ufrn.edu.br>
2019-10-14 19:23:28 -0300

    commit com arquivos de testes do professor

---
+++ src/br/ufrn/imd/huffman/entities/CompressorTest.java
@@ -0,0 +1,21 @@
+package br.ufrn.imd.huffman.entities;
+
+import static org.junit.Assert.*;
+
+public class CompressorTest {
+
+    Compressor compressor;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        compressor = new Compressor("testes/teste1.txt");
+    }
+
+
+    
+
+
+
+
+
+}
