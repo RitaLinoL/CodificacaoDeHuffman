f54bebc02b49f70e3f3f5842c44bb8fa454c8b7e
Rita Lopes <ritalino99@gmail.com>
2019-10-15 02:36:53 -0300

    resolvendo warning por declaração de vetor e uso de equals

--- src/br/ufrn/imd/huffman/main/Main.java
+++ src/br/ufrn/imd/huffman/main/Main.java
@@ -13,49 +13,70 @@ import java.util.Map;
 public class Main {
     //TODO implementar ordem de execução
 
-    public static void main(String args []) {
+    public static void main(String [] args) {
 
-        String pathFile = args[1];
-        //String pathFile2 = "C:/Users/Lucimar/IdeaProjects/CodificacaoDeHuffman/tabelaDeSimbolos.edt";
-        //String outputFile = "C:/Users/Lucimar/IdeaProjects/CodificacaoDeHuffman/arquivoComprimido.edz";
-        Compressor compressor = new Compressor(pathFile);
-        HashMap<String, Integer> mapa = compressor.countLetter();
-        Heap fila = new Heap(mapa.size());
+        String option = args[0];
+        System.out.println(option);
 
-        //testando a função turnLettersInNode()
-        fila = compressor.turnLettersInNodes(mapa);
+        if (option.equals("compress")){
+            String fileIn = args[1];
+            String fileCompress = args[2];
+            String fileTable = args[3];
 
-        for (Node x: fila.getMinHeap()){
-            System.out.println(x.getLetter() + " - " + x.getCount());
+            System.out.println(fileIn);
+            System.out.println(fileCompress);
+            System.out.println(fileTable);
+
+
+
+            /*String pathFile2 = "C:/Users/Lucimar/IdeaProjects/CodificacaoDeHuffman/tabelaDeSimbolos.edt";
+            String outputFile = "C:/Users/Lucimar/IdeaProjects/CodificacaoDeHuffman/arquivoComprimido.edz";*/
+
+            Compressor compressor = new Compressor(fileIn);
+            HashMap<String, Integer> mapa = compressor.countLetter();
+            Heap fila = new Heap(mapa.size());
+
+            //testando a função turnLettersInNode()
+            fila = compressor.turnLettersInNodes(mapa);
+
+            for (Node x: fila.getMinHeap()){
+                System.out.println(x.getLetter() + " - " + x.getCount());
+            }
+
+            //testando a função buildTreeCode
+            Tree arvore = new Tree(compressor.buildTreeCode(fila));
+
+            //arvore.getRoot().accessPreOrder(arvore.getRoot());
+
+            //testando a função buildCodeTable
+            String [] tabela = compressor.buildCodeTable(arvore.getRoot());
+
+            for (String x : tabela) {
+                if(x != null){
+                    System.out.println(x);
+                }
+            }
+
+            //testando a funçao getCodeMap
+            /*
+            HashMap<String, String> mapaDeCodigo = compressor.getCodeMap(tabela);
+            for (String x : mapaDeCodigo.keySet()) {
+                System.out.println(x + mapaDeCodigo.get(x));
+            }*/
+
+            //testando a função storeCodeTable
+            //compressor.storeCodeTable(fileIn, );
+
+            //testando a função storeCodeTextInFile
+            //compressor.storeCodeTextInFile(fileIn, fileCompress, );
+
+
+
+        }else if (option.equals("extract") ){
+            String fileCompress = args[1];
+            String fileTable = args[2];
+            String fileOut = args[3];
         }
-
-        //testando a função buildTreeCode
-        Tree arvore = new Tree(compressor.buildTreeCode(fila));
-
-        //arvore.getRoot().accessPreOrder(arvore.getRoot());
-
-        //testando a função buildCodeTable
-        String tabela[] = compressor.buildCodeTable(arvore.getRoot());
-
-//        for (String x : tabela) {
-//            if(x != null){
-//                System.out.println(x);
-//            }
-//
-//        }
-
-        //testando a funçao getCodeMap
-        /*
-        HashMap<String, String> mapaDeCodigo = compressor.getCodeMap(tabela);
-        for (String x : mapaDeCodigo.keySet()) {
-            System.out.println(x + mapaDeCodigo.get(x));
-        }*/
-
-        //testando a função storeCodeTable
-        //compressor.storeCodeTable(pathFile2, mapaDeCodigo);
-
-        //testando a função storeCodeTextInFile
-        //compressor.storeCodeTextInFile(pathFile, outputFile, mapaDeCodigo);
 
     }
 }
