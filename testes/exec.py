47a4823584ba5e9ca62d61c54e23025ab4f44fe7
Rita Lopes <ritalino99@gmail.com>
2019-10-21 13:20:29 -0300

    adição de pasta de testes

---
+++ testes/exec.py
@@ -0,0 +1,51 @@
+import os
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar compress '/home/bioinfo/Rita/testes/teste1.txt' '/home/bioinfo/Rita/testes/comprimidos/arquivo1.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela1.edt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar compress '/home/bioinfo/Rita/testes/teste2.txt' '/home/bioinfo/Rita/testes/comprimidos/arquivo2.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela2.edt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar compress '/home/bioinfo/Rita/testes/teste3.txt' '/home/bioinfo/Rita/testes/comprimidos/arquivo3.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela3.edt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar compress '/home/bioinfo/Rita/testes/teste4.txt' '/home/bioinfo/Rita/testes/comprimidos/arquivo4.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela4.edt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar compress '/home/bioinfo/Rita/testes/teste5.txt' '/home/bioinfo/Rita/testes/comprimidos/arquivo5.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela5.edt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar compress '/home/bioinfo/Rita/testes/teste6.txt' '/home/bioinfo/Rita/testes/comprimidos/arquivo6.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela6.edt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar compress '/home/bioinfo/Rita/testes/teste7.txt' '/home/bioinfo/Rita/testes/comprimidos/arquivo7.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela7.edt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar compress '/home/bioinfo/Rita/testes/teste8.txt' '/home/bioinfo/Rita/testes/comprimidos/arquivo8.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela8.edt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar compress '/home/bioinfo/Rita/testes/teste9.txt' '/home/bioinfo/Rita/testes/comprimidos/arquivo9.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela9.edt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar compress '/home/bioinfo/Rita/testes/teste10.txt' '/home/bioinfo/Rita/testes/comprimidos/arquivo10.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela10.edt'")
+
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar extract '/home/bioinfo/Rita/testes/comprimidos/arquivo1.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela1.edt' '/home/bioinfo/Rita/testes/gerados/teste1.txt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar extract '/home/bioinfo/Rita/testes/comprimidos/arquivo2.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela2.edt' '/home/bioinfo/Rita/testes/gerados/teste2.txt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar extract '/home/bioinfo/Rita/testes/comprimidos/arquivo3.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela3.edt' '/home/bioinfo/Rita/testes/gerados/teste3.txt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar extract '/home/bioinfo/Rita/testes/comprimidos/arquivo4.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela4.edt' '/home/bioinfo/Rita/testes/gerados/teste4.txt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar extract '/home/bioinfo/Rita/testes/comprimidos/arquivo5.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela5.edt' '/home/bioinfo/Rita/testes/gerados/teste5.txt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar extract '/home/bioinfo/Rita/testes/comprimidos/arquivo6.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela6.edt' '/home/bioinfo/Rita/testes/gerados/teste6.txt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar extract '/home/bioinfo/Rita/testes/comprimidos/arquivo7.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela7.edt' '/home/bioinfo/Rita/testes/gerados/teste7.txt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar extract '/home/bioinfo/Rita/testes/comprimidos/arquivo8.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela8.edt' '/home/bioinfo/Rita/testes/gerados/teste8.txt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar extract '/home/bioinfo/Rita/testes/comprimidos/arquivo9.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela9.edt' '/home/bioinfo/Rita/testes/gerados/teste9.txt'")
+
+os.system("java -jar /home/bioinfo/Rita/CodificacaoDeHuffman/out/artifacts/CodificacaoDeHuffman_jar/CodificacaoDeHuffman.jar extract '/home/bioinfo/Rita/testes/comprimidos/arquivo10.edz' '/home/bioinfo/Rita/testes/comprimidos/tabela10.edt' '/home/bioinfo/Rita/testes/gerados/teste10.txt'")
+
+for x in range(1,11):
+	os.system("diff '/home/bioinfo/Rita/testes/gerados/teste"+str(x)+".txt' '/home/bioinfo/Rita/testes/teste"+str(x)+".txt'")
+
+
+
+
+
+
